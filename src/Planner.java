c8d76ede14af83d2a21338e7c8508f629b397b75
Mia Bongiorno <mab10219@nyu.edu>
2026-04-29 13:12:50 -0400

    started planner.java (added arrayLists)

---
+++ src/Planner.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+/*
+    * Planner.java:
+    * holds lists of courses and assignments and methods to add/remove courses and assignments
+    * Authors: Mia Bongiorno and Allison Tang
+    
+*/
+public class Planner{
+    private ArrayList<Course> courses;
+    private ArrayList<Assignment> assignments;
+
+    
+    public Planner(){
+        courses = new ArrayList<>();
+        assignments = new ArrayList<>();
+    }
+
+}
