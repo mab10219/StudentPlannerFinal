97ed01c9c5da9d5e169aed2b77975792eb09cb9c
Mia Bongiorno <mab10219@nyu.edu>
2026-04-29 15:00:32 -0400

    add list getters and add/delete methods to planner.java

--- src/Planner.java
+++ src/Planner.java
@@ -14,5 +14,40 @@ public class Planner{
         courses = new ArrayList<>();
         assignments = new ArrayList<>();
     }
+    //getters for courses and assignments:
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+    public ArrayList<Assignment> getAssignments() {
+        return assignments;
+    }
+
+    // add courses and assignments to the planner:
+    public void addCourse(Course course){
+        courses.add(course);
+    }
+    public void addAssignment(Assignment assignment){
+        assignments.add(assignment);
+    }
+
+    //removes assignment from the planner by name:
+    public void removeAssignment(String name){
+        for (int i = 0; i <assignments.size(); i++){
+            if (assignments.get(i).getName().equalsIgnoreCase(name)){
+                assignments.remove(i);
+                return;
+            }
+        }
+    }
+    //removes entire course from planner by name:
+    public void removeCourse(String name){
+        for( int i= 0; i <courses.size(); i++){
+            if (courses.get(i).getName().equalsIgnoreCase(name)){
+                courses.remove(i);
+                return;
+            }
+        }
+    }
+   
 
 }
